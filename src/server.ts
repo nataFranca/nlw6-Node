04bbeec62ff5a91ac55751ced4a13bf8ac195d92
Carlos Natã França da Silva <carlosfs0507@gmail.com>
2021-06-23 01:48:56 -0300

    aula1

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+import express, { request, response } from 'express';
+
+const app = express();
+
+/**
+ * GET    -> Buscar uma informação;  
+ * POST   -> Inserir (criar) uma informação;
+ * PUT    -> Alterar uma informação;
+ * DELETE -> Remover um dado;
+ * PATCH  -> Alterar uma informação específica. 
+ */
+
+app.get("/test", (request, response) => {
+  // Request => Entrando
+  // Response => Saindo
+  return response.send("Olá NLW")
+})
+
+app.post("/test-post", (request, response) => {
+  return response.send("Olá NLW método POST")
+})
+
+// http://localhost:3000
+app.listen(3000, () => console.log("Server is running"));
