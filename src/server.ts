d9c9824e9e4c88965fda205e0c0ff707421d96d2
Carlos Natã França da Silva <carlosfs0507@gmail.com>
2021-06-28 01:49:04 -0300

    aula3_aula4

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from 'express';
+import express, { NextFunction, Request, Response } from 'express';
+import "express-async-errors";
 
 import { router } from "./routes";
 
@@ -10,5 +11,20 @@ const app = express();
 app.use(express.json());
 
 app.use(router);
+
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+  //Esse if trata os erros lançados nos services, baseado nas regras definidas para a app
+  if(err instanceof Error) {
+    return response.status(400).json({
+      error: err.message
+    })
+  }
+
+  //Qualquer outra erro que aconteça cai aqui
+  return response.status(500).json({
+    status: "error",
+    message: "Internal Server Error"
+  })
+})
 
 app.listen(3000, () => console.log("Server is running"));
---
+++ src/service/AuthenticateUserService.ts
@@ -0,0 +1,50 @@
+import { getCustomRepository } from "typeorm"
+import { UsersRepositories } from "../repositories/UsersRepositories"
+
+import { compare } from "bcryptjs"
+import { sign } from "jsonwebtoken"
+
+
+interface IAuthenticateRequest {
+  email: string;
+  password: string;
+}
+
+class AuthenticateUserService {
+
+  async execute({ email, password }: IAuthenticateRequest) {
+    const usersRepositories = getCustomRepository(UsersRepositories)
+
+    //Verificar se email existe
+    const user = await usersRepositories.findOne({
+      email
+    });
+
+    if (!user) {
+      throw new Error("Email/Password incorrect")
+    }
+
+    //Verificar se a senha esta correta
+    const passwordMatch = await compare(password, user.password);
+
+    if (!passwordMatch) {
+      throw new Error("Email/Password incorrect")
+    }
+
+    //Gerar token
+    const token = sign(
+      {
+        email: user.email
+      }, 
+      "b30b20097cecaf49b625937a8e308645", //criado com MD5 Generator
+      {
+        subject: user.id,
+        expiresIn: "1d"
+      }
+    );
+
+    return token;
+  }
+}
+
+export { AuthenticateUserService }
